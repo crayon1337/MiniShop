ece51bbb7d8012548caac2eb066e8aa939b26a02
Crayon <darkspear123@outlook.com>
2020-12-29 03:16:20 +0200

    Limited user requests to 30 per minute. Adjusted routes (Basically disabled Jetstream & Fortify routes and re-added them to web.php FOR LOCALIZATION)

--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -30,6 +30,18 @@
             @auth
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
+                <div class="dropdown inline-block relative py-3 mr-2">
+                    <button class="bg-gray-300 text-gray-700 font-semibold py-2 px-6 rounded inline-flex items-center">
+                      <span class="mr-1">{{ __('misc.languages') }}</span>
+                      <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/> </svg>
+                    </button>
+                    <ul class="dropdown-menu absolute hidden text-gray-700 pt-1">
+                        
+                      @foreach($languages as $language)
+                        <li><a class="bg-gray-200 hover:bg-gray-400 py-2 px-4 inline-flex items-center w-36" href="{{ LaravelLocalization::getLocalizedURL($language->locale, null, [], true) }}"><img class="w-4 h-4 mr-2" src="{{ $language->image_url }}" /> {{ $language->title }}</a></li>
+                      @endforeach
+                    </ul>
+                </div>
                 <x-jet-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
